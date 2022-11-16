030c2e0c343d0cd3ce7270596313fb2e5b4acde3
blackendless22 <ads20a00173y@ait.edu.gh>
2022-11-16 01:48:11 +0000

    new file:   command.sh modified:   public/index.html modified:   src/App.js new file:   src/Vector.svg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Vector.svg';
 import './App.css';
 
 function App() {
@@ -6,12 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=" https://www.propelleraero.com/dirtmate/"
           target="_blank"
           rel="noopener noreferrer"
         >
